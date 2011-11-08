6746e19b7a7079ff28f519185f8f71ae1b5cf71c
Anne Rai <anneprai@yahoo.com>
2011-11-08 10:49:18 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makemap::Application.config.secret_token = '5418ee4dab05804e8f5ada06b5ae2085b9043ae8871b8714fcb243a3874926a07d228c828ea30fae5aa6e8d88f50b480daab1649baa4de8fdaf505abece84ec7'
